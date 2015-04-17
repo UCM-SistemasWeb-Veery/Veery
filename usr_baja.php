f67239f5d9060e5d7d44443f170eaddc9bb9e08f
Kuronogard <danielpintoriv@gmail.com>
2015-04-17 22:50:15 +0200

    Added user pages

--- usr_baja.php
+++ usr_baja.php
@@ -2,17 +2,17 @@
 	include_once('includes/header.php');
 ?>
 		<div class="row">
-			<div id="right-sidebar" class="col col_2">
+			<div id="right-sidebar" class="col col-2">
 				<?php
 					include_once('includes/left_sidebar.php');
 				?>
 			</div>
 
-			<div id="content-handler" class="col col_7">
+			<div id="content-handler" class="col col-7">
 				<!-- CONTENIDO DE LA PÁGINA -->				
 			</div>
 			
-			<div id="left-sidebar" class="col col_3">
+			<div id="left-sidebar" class="col col-3">
 				<?php
 					include_once('includes/right_sidebar.php');
 				?>
--- usr_registrado_main.php
+++ usr_registrado_main.php
@@ -2,74 +2,74 @@
 	include_once('includes/header.php');
 ?>
 		<div class="row">
-			<div id="right-sidebar" class="col col_2">
+			<div id="right-sidebar" class="col col-2">
 				<?php
 					include_once('includes/left_sidebar.php');
 				?>
 			</div>
 
-			<div id="content-handler" class="col col_7">
+			<div id="content-handler" class="col col-7">
 				<div id="userspace-header" class="row">
-					<img class="col col_4" src="../img/koala.jpg">
+					<img class="col col-4" src="../img/koala.jpg">
 
-					<h4 class="col col_4">John Doe</h2>
+					<h4 class="col col-4">John Doe</h2>
 
-					<div class="col col_4">
+					<div class="col col-4">
 						<div class="row">
-							<button type="button" class="col col_7 col_offset_2">Darse de baja</button>
+							<button type="button" class="col col-7 col_offset-2">Darse de baja</button>
 						</div>
 						<div class="row">
-							<button type="button" class="col col_7 col_offset_2">modificar perfil</button>
+							<button type="button" class="col col-7 col_offset-2">modificar perfil</button>
 						</div>
 					</div>
 				</div>	
 
 				<div id="userspace-content" class="row">
-					<div class="col col_3">
+					<div class="col col-3">
 						<div  class="row">
-							<img class="col col_9 col_offset_1" src="../img/disco1.bmp">
+							<img class="col col-9 col-offset-1" src="../img/disco1.bmp">
 						</div>
 						<div  class="row">
-							<p class="tracklist-title col col_12">Pink Floyd</p>
+							<p class="tracklist-title col col-12">Pink Floyd</p>
 						</div>
 						<div  class="row">
-							<p class="tracklist-author col col_12">Dark side of the moon</p>
+							<p class="tracklist-author col col-12">Dark side of the moon</p>
 						</div>		
 					</div>
 
-					<div class="col col_3">
+					<div class="col col-3">
 						<div  class="row">
-							<img class="col col_9 col_offset_1" src="../img/disco2.bmp">
+							<img class="col col-9 col-offset-1" src="../img/disco2.bmp">
 						</div>
 						<div  class="row">
-							<p class="tracklist-title col col_12">Guns and roses</p>
+							<p class="tracklist-title col col-12">Guns and roses</p>
 						</div>
 						<div  class="row">
-							<p class="tracklist-description col col_12">Appettite for destruction</p>
+							<p class="tracklist-description col col-12">Appettite for destruction</p>
 						</div>		
 					</div>
 
-					<div class="col col_3">
+					<div class="col col-3">
 						<div  class="row">
-							<img class="col col_9 col_offset_1" src="../img/disco3.bmp">
+							<img class="col col-9 col_offset-1" src="../img/disco3.bmp">
 						</div>
 						<div  class="row">
-							<p class="tracklist-title col col_12">Metallica</p>
+							<p class="tracklist-title col col-12">Metallica</p>
 						</div>
 						<div  class="row">
-							<p class="tracklist-description col col_12">Death Magnetic</p>
+							<p class="tracklist-description col col-12">Death Magnetic</p>
 						</div>		
 					</div>
 
-					<div class="col col_3">
+					<div class="col col-3">
 						<div  class="row">
-							<img class="col col_9 col_offset_1" src="../img/disco4.bmp">
+							<img class="col col-9 col-offset-1" src="../img/disco4.bmp">
 						</div>
 						<div  class="row">
-							<p class="tracklist-title col col_12">Led zeppelin</p>
+							<p class="tracklist-title col col-12">Led zeppelin</p>
 						</div>
 						<div  class="row">
-							<p class="tracklist-description col col_12">Led zeppelin IV</p>
+							<p class="tracklist-description col col-12">Led zeppelin IV</p>
 						</div>		
 					</div>
 
