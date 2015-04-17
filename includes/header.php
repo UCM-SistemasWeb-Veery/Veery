d86ddbf94b9b524fd886c071e2959588f0aac00e
rafaramirez <raffe90@gmail.com>
2015-04-17 17:52:20 +0200

    grid.less improvementes. Left sidebar basic functionality.

---
+++ includes/header.php
@@ -0,0 +1,53 @@
+<?php
+	include_once('constants.php');
+?>
+<!DOCTYPE html>
+<!-- HTML5 Boilerplate -->
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="es-ES>"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="es-ES>"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="es-ES"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="es-ES"> <!--<![endif]-->
+<head>
+
+	<!-- Site meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Rafael Ramirez Urbina (www.fundaseth.es)">
+	<meta name="designer" content="Rafael Ramirez Urbina (www.fundaseth.es)">
+	<title><?php echo SITETITLE; ?></title> 
+	<meta name="description" content="">
+	<!-- <title>Veery</title>-->
+	<link href="css/grid.css" rel="stylesheet" type="text/css">
+	<link href="css/custom.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<div class="container-full">
+		<header>
+		<div id="logo-placeholder">
+			<div class="container">
+			  <img id="logo" src="img/veryLogo.png" alt="Veery">
+			</div>
+		</div>
+		</header><!-- header role="banner" -->
+	</div><!-- container-fluid -->
+<div id="app-content" class="container">
+	<div class="container-full">
+		<nav class="navbar navbar-your-class navbar-static-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"></button> <a class="brand" href="#">Your Logo</a>
+			        <div class="nav-collapse collapse">
+			        	<ul class="nav">
+			            	<li class="active"><a href="#">Home</a></li>
+			                <li><a href="#about">About</a></li>
+			                <li><a href="#contact">Contact</a></li>
+			             </ul>
+			     	</div><!--/.nav-collapse -->
+			     </div>
+			</div>
+		</nav>
+	</div>
---
+++ includes/right_sidebar.php
@@ -0,0 +1,3 @@
+<div class="row">
+	<div class="col col_10 col_offset_1"></div>
+</div>
