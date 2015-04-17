ff265eb12630a96328a03fe4c1ace84b087ae61c
Kuronogard <danielpintoriv@gmail.com>
2015-04-17 22:35:49 +0200

    Basic usr_main page

--- usr_registrado_main.php
+++ usr_registrado_main.php
@@ -10,19 +10,69 @@
 
 			<div id="content-handler" class="col col_7">
 				<div id="userspace-header" class="row">
-					<img class="col col_3" src="../img/koala.jpg">
+					<img class="col col_4" src="../img/koala.jpg">
 
-					<p class="col col_6">
-						Nombre de usuario
-					</p>
+					<h4 class="col col_4">John Doe</h2>
 
-					<p class="col col_3">
-						botones
-					</p>
+					<div class="col col_4">
+						<div class="row">
+							<button type="button" class="col col_7 col_offset_2">Darse de baja</button>
+						</div>
+						<div class="row">
+							<button type="button" class="col col_7 col_offset_2">modificar perfil</button>
+						</div>
+					</div>
 				</div>	
 
 				<div id="userspace-content" class="row">
-					CONTENIDO
+					<div class="col col_3">
+						<div  class="row">
+							<img class="col col_9 col_offset_1" src="../img/disco1.bmp">
+						</div>
+						<div  class="row">
+							<p class="tracklist-title col col_12">Pink Floyd</p>
+						</div>
+						<div  class="row">
+							<p class="tracklist-author col col_12">Dark side of the moon</p>
+						</div>		
+					</div>
+
+					<div class="col col_3">
+						<div  class="row">
+							<img class="col col_9 col_offset_1" src="../img/disco2.bmp">
+						</div>
+						<div  class="row">
+							<p class="tracklist-title col col_12">Guns and roses</p>
+						</div>
+						<div  class="row">
+							<p class="tracklist-description col col_12">Appettite for destruction</p>
+						</div>		
+					</div>
+
+					<div class="col col_3">
+						<div  class="row">
+							<img class="col col_9 col_offset_1" src="../img/disco3.bmp">
+						</div>
+						<div  class="row">
+							<p class="tracklist-title col col_12">Metallica</p>
+						</div>
+						<div  class="row">
+							<p class="tracklist-description col col_12">Death Magnetic</p>
+						</div>		
+					</div>
+
+					<div class="col col_3">
+						<div  class="row">
+							<img class="col col_9 col_offset_1" src="../img/disco4.bmp">
+						</div>
+						<div  class="row">
+							<p class="tracklist-title col col_12">Led zeppelin</p>
+						</div>
+						<div  class="row">
+							<p class="tracklist-description col col_12">Led zeppelin IV</p>
+						</div>		
+					</div>
+
 				</div>
 			</div>
 			
