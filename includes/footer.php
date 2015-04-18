242654be25295d4be49578e4cace97554fe2b41c
rafaramirez <raffe90@gmail.com>
2015-04-18 02:45:34 +0200

    Working on posts. General Layout seems set.

--- includes/footer.php
+++ includes/footer.php
@@ -39,6 +39,55 @@
         		}
     		});
 		});
+
+		equalheight = function(container){
+
+		var currentTallest = 0,
+		     currentRowStart = 0,
+		     rowDivs = new Array(),
+		     $el,
+		     topPosition = 0;
+		 $(container).each(function() {
+
+		   $el = $(this);
+		   $($el).height('auto')
+		   topPostion = $el.position().top;
+
+		   if (currentRowStart != topPostion) {
+		     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+		       rowDivs[currentDiv].height(currentTallest);
+		     }
+		     rowDivs.length = 0; // empty the array
+		     currentRowStart = topPostion;
+		     currentTallest = $el.height();
+		     rowDivs.push($el);
+		   } else {
+		     rowDivs.push($el);
+		     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+		  }
+		   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+		     rowDivs[currentDiv].height(currentTallest);
+		   }
+		 });
+		}
+
+		$(window).load(function() {
+		  equalheight('.top-row .post');
+		});
+
+
+		$(window).resize(function(){
+		  equalheight('.top-row .post');
+		});	
+
+		$(window).load(function() {
+		  equalheight('.bottom-row .post');
+		});
+
+
+		$(window).resize(function(){
+		  equalheight('.bottom-row .post');
+		});				
 	</script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,8 +1,55 @@
 <?php
 	include_once('includes/header.php');
 ?>
-					<div id="content-handler" class="col col-7">
-																		
+					<div id="content-handler" class="col col-9">
+						<div class="row">
+							<div class="col col-12">
+								<div class="row top-row">
+									<div class="main post col col-8">
+										<div class="overlay"></div>
+										<img class="col-12" src="img/rhcp.jpg">
+										<h2 id="main-title">Red Hot Chili Peppers</h2>
+										<a class="btn btn-primary main-button">Live Session</a>
+									</div>
+									<div class="col col-2 post social-post vertical-post">
+										<div class="post-header">
+											<div class="row">
+												<div class="col col-6">
+													<img src="img/pitbull.png">
+												</div>
+												<div class="col col-6">
+													<span class="fa fa-play fa-2x"></span>
+												</div>
+											</div>
+											<div class="row">
+												<p>Time of our lives</p>
+												<p>Pitbull &amp; NeYo</p>
+											</div>	
+										</div>
+										<div class="post-content">
+											<p>Me encanta! No puedo parar de escucharla</p>
+										</div>
+										<div class="post-meta">
+											<p>by <span class="bold">Alberto Miedes</span></p>
+										</div>
+									</div>
+									<div class="col col-2 post famous-post vertical-post">
+										
+									</div>									
+								</div>
+								<div class="row bottom-row">
+									<div class="col col-4 post social-post horizontal-post">
+										
+									</div>									
+									<div class="col col-4 post famous-post horizontal-post">
+										
+									</div>
+									<div class="col col-4 post video-post horizontal-post">
+										<img class="col-12" src="img/franz.jpg">
+									</div>																												
+								</div>
+							</div>
+						</div>												
 					</div>	
 <?php
 	include_once('includes/footer.php');
