dea1afd5a4f978e38130a7a75108da3d24b0b3e8
albertomg994 <albertomg994@gmail.com>
2015-04-19 17:33:39 +0200

    Correcciones importantes pto.6 (alberto)
    Cambios efectuados:
    1. Añadido a custom.css la clase videoWrapper para hacer los iframe
    (embeds de YouTube) responsive.
    2. Eliminados de los .php atributos como width, height, size, cols y
    rows que deberían estar en el .css en vez de aquí.
    3. Corrección de errores tipográficos debido a copy-pastes hechos sin
    cuidado entre diferentes partes de mi código.
    4. Corregido un mal uso de los “row” en los formularios que provocaba
    errores de visualización en chrome.
    5. Eliminadas las clases float-left de los input de tipo “select” y
    “textarea” que provocaban una mala visualización en firefox.
    6. Corregidas las etiquetas input, que finalizaban con “</input>” en
    vez de “/>”

--- empezar_ensayo.php
+++ empezar_ensayo.php
@@ -9,80 +9,100 @@
 			</div>
 			<div id="content-handler" class="col col-7">
 				<div class="row">
+					<!-- div: col col-12 -->
 					<div class="col col-12">
 						<!-- Formulario con información sobre el archivo -->
 						<form action="algo_que_procesa_el_formulario.php" method="post" enctype="multipart/form-data">
 							<fieldset>
 								<legend><h3>Empezar un ensayo en directo</h3></legend>
-								<div>
+								<!-- row Nombre emisión -->
+								<div class="row">
 									<div class="col col-6">
 										<label class="float-right">Nombre de la emisión:</label> 
 									</div>
 									<div class="col col-6">
-										<input class="float-left" type="text" name="nombre_emision" value="Enayo en Paraninfo UCM"></input>
+										<input class="float-left" type="text" name="nombre_ensayo" value="Enayo en Paraninfo UCM">
 									</div>
 								</div>
-								<div>
+								<!-- ./ row Nombre emisión -->
+								<!-- row Ubicación -->
+								<div class="row">
 									<div class="col col-6">
 										<label class="float-right">Ubicación:</label>
 									</div>
 									<div class="col col-6">
-										<input class="float-left" type="text" name="ubicacion" value="Madrid"></input>
+										<input class="float-left" type="text" name="ubicacion" value="Madrid">
 									</div>
 								</div>
-								<div>
+								<!-- ./ row Ubicación -->
+								<!-- row Participantes -->
+								<div class="row">
 									<div class="col col-6">
 										<label class="float-right">Participantes:</label>
 									</div>
 									<div class="col col-6">
-										<input class="float-left" type="text" name="participantes" value="Green Cold Chilli Peppers & Jay-Ñ"></input>
+										<input class="float-left" type="text" name="participantes" value="Red Hot Chili Peppers">
 									</div>
 								</div>
-								<div>
+								<!-- ./ row Participantes -->
+								<!-- row Hora finalización -->
+								<div class="row">
 									<div class="col col-6">
 										<label class="float-right">Hora estimada de finalización:</label>
 									</div>
 									<div class="col col-6">
-										<input class="float-left" type="text" name="hora_fin" value="21:45"></input>
+										<input class="float-left" type="text" name="hora_fin" value="21:45">
 									</div>
 								</div>
-								<div>
+								<!-- ./ row Hora finalización -->
+								<!-- row Género -->
+								<div class="row">
 									<div class="col col-6">
 										<label class="float-right">Género:</label>
 									</div>
 									<div class="col col-6">
-										<select class="float-left" name="genero">
-										  <option value="Rock">Rock</option>
+										<select name="genero">
+										  <option value="Rock" selected>Rock</option>
 										  <option value="Pop">Pop</option>
 										  <option value="Hip-Hop">Hip-Hop</option>
-										  <option value="Reggaeton" selected>Reggaeton</option>
+										  <option value="Reggaeton">Reggaeton</option>
 										  <option value="Clásica">Clásica</option>
 										  <option value="Folk">Folk</option>
 										</select>
 									</div>
 								</div>
-								<div>
+								<!-- ./ row Género-->
+								<!-- row Descripción -->
+								<div class="row">
 									<div class="col col-6">
 										<label class="float-right">Descripción:</label>
 									</div>
 									<div class="col col-6">
-										<textarea class="float-left" name="nombre" rows="4" cols="40" >Escribe aquí tu comentario</textarea>
+										<textarea class="float-left" name="descripcion">Escribe aquí tu comentario</textarea>
 									</div>
 								</div>
-								
-								<div>
-									<!--Previsualización:-->
-									<!-- Poner en la css del image el max width etc. y luego meterlo en un container -->
-									<iframe src="https://www.youtube.com/embed/D2YIbo-uwhk" width="640" height="480"></iframe>
-								</div>
-								<div>
+								<!-- ./ row Descripción -->
+								<!-- row Previsualización -->
+								<div class="row">
 									<div class="col col-12">
-										<input class="btn btn-lg btn-orange float-right" type="submit" name="enviar" value="EMPEZAR"/>
+										<div class="videoWrapper">
+											<iframe src="https://www.youtube.com/embed/D2YIbo-uwhk" frameborder="0" allowfullscreen></iframe>
+										</div>
 									</div>
 								</div>
+								<!-- ./ row Previsualización -->
+								<!-- row Empezar emisión -->
+								<div class="row">
+									<div class="col col-12">
+										<input class="btn float-right" type="submit" name="empezar_emision" value="EMPEZAR">
+									</div>
+								</div>
+								<!-- ./ row Empezar emisión -->
 							</fieldset>
 						</form>
-					</div>																
+						<!-- ./ Formulario con información sobre el archivo -->
+					</div>
+					<!-- ./ div: col col-12 -->																
 				</div>				
 			</div>
 			
