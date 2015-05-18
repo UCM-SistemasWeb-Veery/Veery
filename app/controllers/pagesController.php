8f8598aaeb222f342e3b89ab9a26f9fb155d5d33
rafaramirez <raffe90@gmail.com>
2015-05-18 10:55:01 +0200

    Added welcome page. Forced

--- app/controllers/pagesController.php
+++ app/controllers/pagesController.php
@@ -1,6 +1,7 @@
 <?php namespace controllers;
 
-use core\view,
+use \core\view,
+    \core\session as Session,
     \core\controller as Controller;
 
 class PagesController extends Controller{
@@ -14,17 +15,22 @@ class PagesController extends Controller{
      * Define Index page title and load template files
      */
     public function index() {
-        $data['feed'] = $this->_model->getFeed();
-        $data['js'] = "
-            <script>
-              var container = document.querySelector('#feed-grid');
-              var msnry = new Masonry( container, {
-              });
-            </script>
-        ";
-        View::renderpartial('header', $data);
-        View::render('index', $data);
-        View::renderpartial('footer', $data);
+
+        if(!Session::get('loggedin')){
+            View::render('welcome');
+        }else{
+            $data['feed'] = $this->_model->getFeed();
+            $data['js'] = "
+                <script>
+                  var container = document.querySelector('#feed-grid');
+                  var msnry = new Masonry( container, {
+                  });
+                </script>
+            ";
+            View::renderpartial('header', $data);
+            View::render('index', $data);
+            View::renderpartial('footer', $data);
+        }
     }
 
     public function startBroadcast(){
---
+++ app/views/welcome.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<!-- HTML5 Boilerplate -->
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="es-ES>"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="es-ES>"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="es-ES"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="es-ES"> <!--<![endif]-->
+<head>
+
+	<!-- Site meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Rafael Ramirez Urbina (www.fundaseth.es)">
+	<meta name="designer" content="Rafael Ramirez Urbina (www.fundaseth.es)">
+	<title><?php echo 'Welcome | '.SITETITLE; ?></title> 
+	<meta name="description" content="">
+
+	<link href="<?php echo css_path;?>custom.css" rel="stylesheet" type="text/css">
+	<link href="<?php echo css_path;?>player.css" rel="stylesheet" type="text/css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+   
+</head>
+<body>
+
+<div id="banner" class="row">
+    <header>
+    	<div class="row">
+    		<div class="col col-4">
+          <div id="menu-button">
+              <a href="#menu-toggle" id="menu-toggle"><span></span></a>
+          </div> 
+          <a href="<?php echo PATH ?>"><img id="logo" src="<?php echo PATH ?>img/veryLogo.png" alt="Veery"></a>
+        </div>
+    	</div>
+    </header>
+</div>
+<div class="homepage-hero-module">
+    <div class="video-container">
+        <div class="title-container">
+					<div class="col col-3 col-offset-2">
+						<h1>Welcome!</h1>
+						<p>Connect with your friends — and other fascinating people. Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.</p>
+					</div>
+					<div class="col col-4 col-offset-1">
+        		<div class="welcome-form">
+        			<h2>Returning users</h2>
+        			<div id="login-form">
+        				<form action="<?php echo PATH.'login'; ?>" method="post">
+        					<input class="btn inverse small" type="text" name="username" placeholder="Usuario">
+        					<input class="btn inverse small" type="password" name="password" placeholder="Contraseña">
+									<input class="btn alternate small center" type="submit" name="submit" value="acceder">
+        				</form>
+        			</div>
+        		</div>
+        		<div class="welcome-form">
+							<h2>New users</h2>
+							<form id="register" action="<?php echo PATH.'users/new'; ?>" method="post">
+					  		<input class="btn inverse small <?php if(!empty($data['userHandle'])){echo 'error';} ?>" type="text" name="userHandle" placeholder="<?php if(!empty($data['username'])){echo $data['userHandle'];}else{echo 'Usuario';} ?>">
+					  		<input class="btn inverse small <?php if(!empty($data['userEmail'])){echo 'error';} ?>" type="text" name="userEmail" placeholder="<?php if(!empty($data['username'])){echo $data['userHandle'];}else{echo 'Email';} ?>">
+								<input class="btn inverse small" type="password" name="password" placeholder="Contraseña">
+								<input class="btn alternate small center" type="submit" name="submit" value="Registrarse">
+							</form>	        		
+						</div>	
+					</div>					
+        </div>
+        <div class="filter"></div>
+        <video autoplay loop muted class="fillWidth">
+            <source src="http://veery.es/video/uptownfunk.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.</video>
+    </div>
+</div>
+<!-- /container -->
+<!-- 	<div class="homepage-hero-module">
+
+			<div class="video-container">
+        <div id="banner" class="row">
+            <header>
+            	<div class="row">
+            		<div class="col col-4">
+				          <div id="menu-button">
+				              <a href="#menu-toggle" id="menu-toggle"><span></span></a>
+				          </div> 
+				          <a href="<?php echo PATH ?>"><img id="logo" src="<?php echo PATH ?>img/veryLogo.png" alt="Veery"></a>
+			          </div>
+            	</div>
+            </header>
+        </div>
+        <div class="row welcome-party">
+					<div class="col col-4 col-offset-2">
+						<h1>Welcome!</h1>
+						<div class="filter"></div>
+						<video autoplay loop class="fillWidth">
+            <source src="http://veery.es/video/uptownfunk.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.</video>
+					</div>
+					<div class="col col-4">
+        		<div class="welcome-form">
+        			<h2>Returning users</h2>
+        			<div id="login-form">
+        				<form action="<?php echo PATH.'login'; ?>" method="post">
+        					<input class="btn inverse small" type="text" name="username" placeholder="Usuario">
+        					<input class="btn inverse small" type="password" name="password" placeholder="Contraseña">
+									<input class="btn alternate small center" type="submit" name="submit" value="acceder">
+        				</form>
+        			</div>
+        		</div>
+        		<div class="welcome-form">
+							<h2>New users</h2>
+							<form id="register" action="<?php echo PATH.'users/new'; ?>" method="post">
+					  		<input class="btn inverse small <?php if(!empty($data['userHandle'])){echo 'error';} ?>" type="text" name="userHandle" placeholder="<?php if(!empty($data['username'])){echo $data['userHandle'];}else{echo 'Usuario';} ?>">
+					  		<input class="btn inverse small <?php if(!empty($data['userEmail'])){echo 'error';} ?>" type="text" name="userEmail" placeholder="<?php if(!empty($data['username'])){echo $data['userHandle'];}else{echo 'Email';} ?>">
+								<input class="btn inverse small" type="password" name="password" placeholder="Contraseña">
+								<input class="btn alternate small center" type="submit" name="submit" value="Registrarse">
+							</form>	        		
+						</div>	
+					</div>				
+        </div>
+      </div> -->
+      <!-- /#app-content -->
+  </div>
+  <!-- /#wrapper -->
+  <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+  <script type="text/javascript">
+/** Document Ready Functions **/
+/********************************************************************/
+
+$( document ).ready(function() {
+
+    // Resive video
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+        
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+/** Reusable Functions **/
+/********************************************************************/
+
+function scaleVideoContainer() {
+
+    var height = $(window).height();
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+    
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+        windowHeight = $(window).height(),
+        videoWidth,
+        videoHeight;
+    
+    console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width'),
+            windowAspectRatio = windowHeight/windowWidth;
+
+        if (videoAspectRatio > windowAspectRatio) {
+            videoWidth = windowWidth;
+            videoHeight = videoWidth * videoAspectRatio;
+            $(this).css({'top' : -(videoHeight - windowHeight) / 2 + 'px', 'margin-left' : 0});
+        } else {
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+        }
+
+        $(this).width(videoWidth).height(videoHeight);
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+        
+
+    });
+}  
+  </script>
+	</body>
+
+	</html>  
